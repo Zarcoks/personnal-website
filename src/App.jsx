db24ef4b5a4d846c2cd8f57ecf28ce0bbaadcc2b
Victor Jost <96876377+Zarcoks@users.noreply.github.com>
2025-06-14 11:25:32 -0400

    Basic react config with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <h1 className="text-3xl font-bold">Personnal Website</h1>
+    </div>
+  )
+}
+
+export default App
